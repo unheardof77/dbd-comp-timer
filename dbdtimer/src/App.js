88c12d86d6a3052ef96610330581758ca8b33ea0
unheardof77 <morgan.tolman04@gmail.com>
2024-03-31 19:19:39 -1000

    got timer to succesfully work showing minutes seconds and miliseconds.

--- dbdtimer/src/App.js
+++ dbdtimer/src/App.js
@@ -1,25 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [ currentTime, setCurrentTime ] = useState(0);
-  let timerInterval;
-  const startTimer = () =>{
-    timerInterval = setInterval(()=>{
-      console.log("hello")
-      const newTime = currentTime + 10;
-      setCurrentTime(newTime)
-    },10)
-  }
+  const [ isRunning, setIsRunning ] = useState(false);
+  
+  useEffect(() => {
+    let timeInterval;
+    if (isRunning){
+      timeInterval = setInterval(()=>{
+        setCurrentTime(prev => prev +1);
+      }, 10)
+    }
+    return () => clearInterval(timeInterval)
+  }, [isRunning, currentTime])
+
+  const milliseconds = currentTime % 100
+  const seconds = Math.floor((currentTime % 6000) / 100)
+  const minutes = Math.floor((currentTime % 360000) / 6000)
+
 
   const btnClickHandler = () => {
-    startTimer();
+    setIsRunning(!isRunning);
   }
 
 
   return (
     <div className="App">
-      <h1>{currentTime}</h1>
+      <h1>{minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}.{milliseconds.toString().padStart(2, "0")}</h1>
       <button onClick={btnClickHandler} >Start</button>
     </div>
   );
